71eb8aecf2eb88796330c371ca5cae7578a3dfcf
wswj <1360378350@qq.com>
2020-11-29 15:44:25 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:shortvideo/page/home/home-main.dart';
 
 void main() {
-  ///自定义报错页面987
+  ///自定义报错页面987345
   if(kReleaseMode){
     ErrorWidget.builder=(FlutterErrorDetails flutterErrorDetails){
       debugPrint(flutterErrorDetails.toString());
