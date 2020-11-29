dff1a450bc563b1a48f0741a23e8b6dac30af303
wswj <1360378350@qq.com>
2020-11-29 15:14:04 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:shortvideo/page/home/home-main.dart';
 
 void main() {
-  ///自定义报错页面
+  ///自定义报错页面54321
   if(kReleaseMode){
     ErrorWidget.builder=(FlutterErrorDetails flutterErrorDetails){
       debugPrint(flutterErrorDetails.toString());
